7ed7aac0c010d03887e47294a42a1558aa86e9c8
Pavel Skobelev <pskobelev@me.com>
2023-12-09 16:46:58 +0300

    FIX: add readme, req, fix some bugs

--- app.py
+++ app.py
@@ -2,22 +2,17 @@ import requests
 from decouple import config
 from urllib.parse import urlparse
 
-BASEURL = 'https://api-ssl.bitly.com/v4'
-HEADERS = {
-    'Authorization': 'Bearer ' + config('BITLY_API_TOKEN'),
-    'Content-Type': 'application/json'
-}
-
 
 def is_bitlink(headers, url):
-    bitlink = urlparse(url)._replace(scheme='').geturl()[2:]
-    bitly_url = f'{BASEURL}/bitlinks/{bitlink}'
+    parser = urlparse(url)
+    bitlink = f"{parser.netloc}{parser.path}"
+    bitly_url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
     response = requests.get(bitly_url, headers=headers)
     return response.ok
 
 
 def shorten_link(headers, url):
-    bitly_url = f'{BASEURL}/shorten'
+    bitly_url = "https://api-ssl.bitly.com/v4/shorten"
     payload = {'long_url': url}
     response = requests.post(bitly_url, headers=headers, json=payload)
     response.raise_for_status()
@@ -25,8 +20,9 @@ def shorten_link(headers, url):
 
 
 def count_clicks(header, url):
-    bitlink = urlparse(url)._replace(scheme='').geturl()
-    bitly_url = f'{BASEURL}/bitlinks/{bitlink}/clicks/summary'
+    parser = urlparse(url)
+    bitlink = f"{parser.netloc}{parser.path}"
+    bitly_url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
     response = requests.get(bitly_url, headers=header)
     response.raise_for_status()
     return response.json()['total_clicks']
@@ -34,15 +30,19 @@ def count_clicks(header, url):
 
 def main():
     user_input = input("Я могу сократить ссылку или показать сколько было кликов\nДавай ссылку: ")
+
+    token = config('BITLY_API_TOKEN')
+    headers = {'Authorization': f"Bearer {token}"}
+
     try:
-        if is_bitlink(HEADERS, user_input):
-            clicks = count_clicks(HEADERS, user_input)
+        if is_bitlink(headers, user_input):
+            clicks = count_clicks(headers, user_input)
             print(f"Всего кликов: {clicks}")
         else:
-            short_url = shorten_link(HEADERS, user_input)
+            short_url = shorten_link(headers, user_input)
             print(f"Битлинк: {short_url}")
     except requests.exceptions.HTTPError as e:
-        exit(f"Ошибочка получается: {e}")
+        print(f"Ошибочка получается: {e}")
 
 
 if __name__ == '__main__':
